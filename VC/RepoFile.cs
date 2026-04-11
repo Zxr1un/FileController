9d4dfe1ee3086346f13fc5c60090a229fa6cc79e
Zxr1un <zakharpodstud@gmail.com>
2026-04-11 20:41:59 +0300

    Добавьте файлы проекта.

---
+++ VC/RepoFile.cs
@@ -0,0 +1,10 @@
+﻿namespace FileController.VC
+{
+    public class RepoFile
+    {
+        public string Path { get; set; } = "";     // относительный путь
+        public string Hash { get; set; } = "";     // SHA256
+        public string StorageId { get; set; } = ""; // имя в storage
+        public bool NeedToStore { get; set; } = false;
+    }
+}
